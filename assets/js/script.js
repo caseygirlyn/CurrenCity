aa58a883bb607c4dadb5934632f6fe59da910616
Girlyn Antillon <casey.girlyn@gmail.com>
2024-02-07 15:49:01 +0000

    refactor code and use restcountries api to fetch currency code

--- assets/js/script.js
+++ assets/js/script.js
@@ -165,138 +165,30 @@ function displayForecast(city) {
             return response.json();
           }).then(function (result) {
 
-            if (countryCode == 'BE' || countryCode == 'BG' || countryCode == 'CZ' || countryCode == 'DK' || countryCode == 'DE' || countryCode == 'EE' || countryCode == 'IE' || countryCode == 'EL' || countryCode == 'ES' || countryCode == 'FR' || countryCode == 'HR' || countryCode == 'IT' || countryCode == 'CY' || countryCode == 'LV' || countryCode == 'LT' || countryCode == 'LU' || countryCode == 'HU' || countryCode == 'MC' || countryCode == 'MT' || countryCode == 'NL' || countryCode == 'AT' || countryCode == 'PL' || countryCode == 'PT' || countryCode == 'RO' || countryCode == 'SI' || countryCode == 'SK' || countryCode == 'FI' || countryCode == 'SE' || countryCode == 'GR' || countryCode == 'ME' || countryCode == 'XK'
-            ) {
-              currency = 'EUR';
-            } else if (countryCode == 'LI' || countryCode == 'CH') {
-              currency = 'CHF';
-            } else if (countryCode == 'US') {
-              currency = 'USD';
-            } else if (countryCode == 'BG') {
-              currency = 'BGN';
-            } else if (countryCode == 'CZ') {
-              currency = 'CZK';
-            } else if (countryCode == 'DK') {
-              currency = 'DKK';
-            } else if (countryCode == 'SG') {
-              currency = 'SGD';
-            } else if (countryCode == 'PH') {
-              currency = 'PHP';
-            } else if (countryCode == 'IS') {
-              currency = 'ISK';
-            } else if (countryCode == 'NO') {
-              currency = 'NOK';
-            } else if (countryCode == 'BA') {
-              currency = 'BAM';
-            } else if (countryCode == 'MD') {
-              currency = 'MDL';
-            } else if (countryCode == 'MK') {
-              currency = 'MKD';
-            } else if (countryCode == 'GE') {
-              currency = 'GEL';
-            } else if (countryCode == 'AL') {
-              currency = 'ALL';
-            } else if (countryCode == 'RS') {
-              currency = 'RSD';
-            } else if (countryCode == 'TR') {
-              currency = 'TRY';
-            } else if (countryCode == 'UA') {
-              currency = 'UAH';
-            } else if (countryCode == 'AM') {
-              currency = 'AMD';
-            } else if (countryCode == 'BY') {
-              currency = 'BYN';
-            } else if (countryCode == 'AZ') {
-              currency = 'AZN';
-            } else if (countryCode == 'DZ') {
-              currency = 'DZD';
-            } else if (countryCode == 'LB') {
-              currency = 'LBP';
-            } else if (countryCode == 'SY') {
-              currency = 'SYP';
-            } else if (countryCode == 'EG') {
-              currency = 'EGP';
-            } else if (countryCode == 'LY') {
-              currency = 'LYD';
-            } else if (countryCode == 'TN') {
-              currency = 'TND';
-            } else if (countryCode == 'IL') {
-              currency = 'ILS';
-            } else if (countryCode == 'MA') {
-              currency = 'MAD';
-            } else if (countryCode == 'JO') {
-              currency = 'JOD';
-            } else if (countryCode == 'PS') {
-              currency = 'ILS';
-            } else if (countryCode == 'AR') {
-              currency = 'ARS';
-            } else if (countryCode == 'AU') {
-              currency = 'AUD';
-            } else if (countryCode == 'BR') {
-              currency = 'BRL';
-            } else if (countryCode == 'CA') {
-              currency = 'CAD';
-            } else if (countryCode == 'CN_X_HK') {
-              currency = 'CNY';
-            } else if (countryCode == 'HK') {
-              currency = 'HKD';
-            } else if (countryCode == 'IN') {
-              currency = 'INR';
-            } else if (countryCode == 'JP') {
-              currency = 'JPY';
-            } else if (countryCode == 'MX') {
-              currency = 'MXN';
-            } else if (countryCode == 'NG') {
-              currency = 'NGN';
-            } else if (countryCode == 'NZ') {
-              currency = 'NZD';
-            } else if (countryCode == 'RU') {
-              currency = 'RUB';
-            } else if (countryCode == 'SG') {
-              currency = 'SGD';
-            } else if (countryCode == 'ZA') {
-              currency = 'ZAR';
-            } else if (countryCode == 'KR') {
-              currency = 'KRW';
-            } else if (countryCode == 'TW') {
-              currency = 'TWD';
-            } else if (countryCode == 'TH') {
-              currency = 'THB';
-            } else if (countryCode == 'AE') {
-              currency = 'AED';
-            } else if (countryCode == 'MO') {
-              currency = 'MOP';
-            } else if (countryCode == 'MY') {
-              currency = 'MYR';
-            } else if (countryCode == 'CN') {
-              currency = 'CNY';
-            } else if (countryCode == 'IN') {
-              currency = 'INR';
-            } else if (countryCode == 'BB') {
-              currency = 'BBD';
-            } else if (countryCode == 'BD') {
-              currency = 'BDT';
-            } else if (countryCode == 'BS') {
-              currency = 'BSD';
-            } else if (countryCode == 'BZ') {
-              currency = 'BZD';
-            } else if (countryCode == 'CO') {
-              currency = 'COP';
-            } else if (countryCode == 'CR') {
-              currency = 'CRC';
-            }
+            let queryCountryCode = 'https://restcountries.com/v3.1/alpha/' + countryCode;
+            fetch(queryCountryCode)
+              .then(function (response) {
+                return response.json();
+              }).then(function (resultCountryCode) {
 
-            if (currency) {
-              let currencyRate = result.rates[currency].toFixed(2);
+                // Convert Object to String
+                let resultCC = JSON.stringify(resultCountryCode[0].currencies);
 
-              if (currencyRate) {
-                let currencyData = `<p class='currencyText rounded-1'>1 GBP = ${currencyRate} ${currency}</p>`;
-                currencyExchangeDiv.append(currencyData);
-              } else {
-                currencyExchangeDiv.empty();
-              }
-            }
+                // Get the Currency Code
+                currency = resultCC.split('"')[1];
+                
+                if (currency) {
+                  let currencyRate = result.rates[currency].toFixed(2);
 
+                  if (currencyRate) {
+                    let currencyData = `<p class='currencyText rounded-1'>1 GBP = ${currencyRate} ${currency}</p>`;
+                    currencyExchangeDiv.append(currencyData);
+                  } else {
+                    currencyExchangeDiv.empty();
+                  }
+                }
+
+              });
           });
       }
 
